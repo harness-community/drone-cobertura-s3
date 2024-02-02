e931150661e617c0d1f58aff23f6e2e508399a6f
Shivam Srivastava <shivam.362000.1@gmail.com>
2024-02-02 12:58:38 +0530

    Build scripts added

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,4 +9,9 @@ export CGO_ENABLED=0
 set -e
 set -x
 
-GOOS=linux GOARCH=amd64 go build -o release/linux/amd64/drone-cobertura-s3   .
+# linux
+GOOS=linux GOARCH=amd64 go build -o release/linux/amd64/drone-cobertura-s3 .
+GOOS=linux GOARCH=arm64 go build -o release/linux/arm64/drone-cobertura-s3 .
+
+# windows
+GOOS=windows go build -o release/windows/amd64/drone-cobertura-s3.exe
